9051174c56620bf34437065a9734e5c3e0e96568
Albert Wen <albertzwen@gmail.com>
2022-10-13 00:18:17 -0700

    able to simulate the equations in their entirety

--- hw1/lorenz.m
+++ hw1/lorenz.m
@@ -1,11 +1,17 @@
-function f = lorenz(x1, x2, x3, sigma, b, r)
+function f = lorenz(x)
 %Simulates based on the Lorenz equation 
 %   Algorithm 10.4
-if vargin == 3
-    sigma = 4;
-    b = 1;
-    r = 48;
-end
+% if varargin == 3
+%     sigma = 4;
+%     b = 1;
+%     r = 48;
+% end
+sigma = 4;
+b = 1;
+r = 48;
+x1 = x(1);
+x2 = x(2);
+x3 = x(3);
 f = [sigma * (x2 - 1), ...
     -x2 - x1 * x3, ...
     -b * x3 + x1 * x2 - b * r];
--- hw1/rossler.m
+++ hw1/rossler.m
@@ -1,12 +1,18 @@
-function f = rossler(x1, x2, x3, a, b, c)
+function f = rossler(x)
 %Simulates based on the Rossler equation 
 %   Algorithm 10.4
-if vargin == 3
-    a = 0.2;
-    b = 0.2;
-    c = 5.7;
-end
-f = [-x2 - x3, 
-    x1 + a * x2,
+% if varargin == 3
+%     a = 0.2;
+%     b = 0.2;
+%     c = 5.7;
+% end
+a = 0.2;
+b = 0.2;
+c = 5.7;
+x1 = x(1);
+x2 = x(2);
+x3 = x(3);
+f = [-x2 - x3, ...
+    x1 + a * x2, ...
     b + x3 * (x1 - c)];
 end
