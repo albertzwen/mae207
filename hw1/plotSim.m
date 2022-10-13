f87f3ec64efb99456ce16e938108ef4e327ed0de
Albert Wen <albertzwen@gmail.com>
2022-10-13 00:33:38 -0700

    plotting of simulated figures completed

--- hw1/plotSim.m
+++ hw1/plotSim.m
@@ -1,7 +1,13 @@
-function fig = plotSim(pos, title, subtitle);
+function fig = plotSim(pos, title_var, subtitle_var)
+cla; clf;
+fig = figure;
 hold on;
-plot3()
+grid on;
 view(-45, 30);
 xlabel("x_1");
 ylabel("x_2");
 zlabel("x_3");
+title(title_var);
+subtitle(subtitle_var);
+plot3(pos(:, 1), pos(:, 2), pos(:, 3), 'MarkerSize', 5);
+
