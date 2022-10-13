9f303048c78e38d54dff637ffdc431a87eb52699
Albert Wen <albertzwen@gmail.com>
2022-10-12 23:04:53 -0700

    Everything I need is right here I think

--- hw1/lorenz.m
+++ hw1/lorenz.m
@@ -1,6 +1,6 @@
 function f = lorenz(x1, x2, x3, pStruct)
 %Simulates based on the Lorenz equation 
-%   Needs work
+%   Algorithm 10.4
 f = [pStruct.sigma * (x2 - 1); ...
     -x2 - x1 * x3; ...
     -pstruct.b * x3 + x1 * x2 - pStruct.b * pStruct.r];
--- hw1/rossler.m
+++ hw1/rossler.m
@@ -1,5 +1,5 @@
 function f = rossler(x1, x2, x3, pStruct)
-%Simulates based on the Lorenz equation 
-%   Needs work
+%Simulates based on the Rossler equation 
+%   Algorithm 10.4
 f = [-x2 - x3; x1 + pStruct.a * x2; pStruct.b + x3(x1 - pStruct.c)];
 end
