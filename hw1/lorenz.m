8760880fd8dedc10094c5b35a34ce3b0ab14a644
Albert Wen <albertzwen@gmail.com>
2022-10-12 23:12:01 -0700

    removal of structure arrays because I don't quite like working with them (yet)

--- hw1/lorenz.m
+++ hw1/lorenz.m
@@ -1,8 +1,8 @@
-function f = lorenz(x1, x2, x3, pStruct)
+function f = lorenz(x1, x2, x3, sigma, b, r)
 %Simulates based on the Lorenz equation 
 %   Algorithm 10.4
-f = [pStruct.sigma * (x2 - 1); ...
+f = [sigma * (x2 - 1); ...
     -x2 - x1 * x3; ...
-    -pstruct.b * x3 + x1 * x2 - pStruct.b * pStruct.r];
+    -b * x3 + x1 * x2 - b * r];
 end
 
--- hw1/rossler.m
+++ hw1/rossler.m
@@ -1,5 +1,7 @@
-function f = rossler(x1, x2, x3, pStruct)
+function f = rossler(x1, x2, x3, a, b, c)
 %Simulates based on the Rossler equation 
 %   Algorithm 10.4
-f = [-x2 - x3; x1 + pStruct.a * x2; pStruct.b + x3(x1 - pStruct.c)];
+f = [-x2 - x3; 
+    x1 + a * x2;
+    b + x3 * (x1 - c)];
 end
