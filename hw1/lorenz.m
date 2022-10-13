26f2e344df015a0aae83e116c5f27b6b4c56e45e
Albert Wen <albertzwen@gmail.com>
2022-10-12 23:46:56 -0700

    inclusion of default values because I don't want to make customization a thing

--- hw1/lorenz.m
+++ hw1/lorenz.m
@@ -1,6 +1,11 @@
 function f = lorenz(x1, x2, x3, sigma, b, r)
 %Simulates based on the Lorenz equation 
 %   Algorithm 10.4
+if vargin == 3
+    sigma = 4;
+    b = 1;
+    r = 48;
+end
 f = [sigma * (x2 - 1), ...
     -x2 - x1 * x3, ...
     -b * x3 + x1 * x2 - b * r];
--- hw1/rossler.m
+++ hw1/rossler.m
@@ -1,6 +1,11 @@
 function f = rossler(x1, x2, x3, a, b, c)
 %Simulates based on the Rossler equation 
 %   Algorithm 10.4
+if vargin == 3
+    a = 0.2;
+    b = 0.2;
+    c = 5.7;
+end
 f = [-x2 - x3, 
     x1 + a * x2,
     b + x3 * (x1 - c)];
