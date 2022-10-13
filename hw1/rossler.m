84abb8b93a8eae0cc5dbf97c00604648ea971b9d
Albert Wen <albertzwen@gmail.com>
2022-10-12 23:21:03 -0700

    making sure the positions form a tall matrix rather than a wide one

--- hw1/rossler.m
+++ hw1/rossler.m
@@ -1,7 +1,7 @@
 function f = rossler(x1, x2, x3, a, b, c)
 %Simulates based on the Rossler equation 
 %   Algorithm 10.4
-f = [-x2 - x3; 
-    x1 + a * x2;
+f = [-x2 - x3, 
+    x1 + a * x2,
     b + x3 * (x1 - c)];
 end
