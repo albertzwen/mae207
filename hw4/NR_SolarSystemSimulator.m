a3c843420f5200d28b6ad1df7e277b1ada8ce2db
Albert Wen <albertzwen@gmail.com>
2022-10-26 12:42:28 -0700

    initial commit

---
+++ hw4/NR_SolarSystemSimulator.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = NR_SolarSystemSimulator(inputArg1,inputArg2)
+%NR_SOLARSYSTEMSIMULATOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
