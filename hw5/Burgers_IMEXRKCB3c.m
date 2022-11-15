f67f1034128035c9de7c268c9a63e8ce25b9d43b
Albert Wen <albertzwen@gmail.com>
2022-11-15 00:46:33 -0800

    some constants defined, struggling to outline the correct logic for IMEXRKCB3c

--- hw5/Burgers_IMEXRKCB3c.m
+++ hw5/Burgers_IMEXRKCB3c.m
@@ -3,28 +3,49 @@ function Burgers_IMEXRKCB3c
 % Simulate the 1D Burgers on 0<x<L with homogeneous Dirichlet BCs using CN/RKW3 in time
 % (explicit on nonlinear terms, implicit on linear terms)
 
-%%%%%%%%%%%%%%%%%%%% Initialize the simulation paramters (user input) %%%%%
-L=100; Tmax=50; N=100; dt=0.5; PlotInterval=1;
-dx=L/N; x=(0:N)*dx; y=-sin(pi*x/L)-sin(2*pi*x/L)+sin(6*pi*x/L); NR_PlotXY(x,y,0,0,L,-3,3)
-%%%%%%%%%%%% Precalculate the time-stepping coefficients used in the simulation %%%%%%%%%%
-h_bar = dt*[8/15 2/15  1/3];     d=h_bar/(2*dx^2);          a= -h_bar/(2*dx^2); 
-beta_bar = [1    25/8  9/4];     e=beta_bar.*h_bar/(2*dx);  b=1+h_bar/dx^2;
-zeta_bar = [0   -17/8 -5/4];     f=zeta_bar.*h_bar/(2*dx);  c= -h_bar/(2*dx^2);
-for k=1:Tmax/dt
-  for rk=1:3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ALL 3 RK SUBSTEPS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-     % Compute nonlinear term r(y), & defer scaling until next step %  Leading-order cost:
-     r=-y(2:N).*(y(3:N+1)-y(1:N-1));                                           % ~ 2N
-     % Compute entire rhs, and apply all the correct scalings
-     if (rk==1)
-        rhs=y(2:N) +d(rk)*(y(3:N+1)-2*y(2:N)+y(1:N-1)) +e(rk)*r;               % ~ 7N
-     else
-        rhs=y(2:N) +d(rk)*(y(3:N+1)-2*y(2:N)+y(1:N-1)) +e(rk)*r +f(rk)*rhs;    % ~ 9N
-     end       
-     % Solve for new y
-     y(2:N)=NR_ThomasTT(a(rk),b(rk),c(rk),rhs',N-1);                              % ~ 8N
-     % Save r (in rhs) for the next timestep                            --------------
-     if (rk<3) rhs=r; end                                               % Total: ~ 55N
-  end %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END OF RK LOOP %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  if (mod(k,PlotInterval)==0) NR_PlotXY(x,y,k*dt,0,L,-3,3); end            
-end                                                                     
+% Initialize the simulation parameters (user input)
+L = 100;
+Tmax = 50;
+N = 100;
+dt = 0.5;
+PlotInterval=1;
+dx = L / N;
+x = (0:N) .* dx; % length N + 1
+y = -sin(pi * x / L) - sin(2 * pi * x / L) + sin(6 * pi * x / L);
+NR_PlotXY(x,y,0,0,L,-3,3)
+% Precalculate the time-stepping coefficients used in the simulation
+% h_bar = dt*[8/15 2/15  1/3];     d=h_bar/(2*dx^2);          a= -h_bar/(2*dx^2);
+% beta_bar = [1    25/8  9/4];     e=beta_bar.*h_bar/(2*dx);  b=1+h_bar/dx^2;
+% zeta_bar = [0   -17/8 -5/4];     f=zeta_bar.*h_bar/(2*dx);  c= -h_bar/(2*dx^2);
+% if last two characters end in bt, it means it's from Butcher tableau
+aexbt = [0, 0, 0; ...
+    3375509829940 / 42525919076317, 0, 0; ...
+    0, 272778623835 / 1039454778728, 0];
+aimbt = [0, 0, 0; ...
+    0, 3375509829940 / 4525919076317, 0;
+    0, 11712383888607531889907 / 32694570495602105556248, 566138307881 / 912153721139];
+bbt = [0, 673488652607 / 2334033219546, 493801219040 / 853653026979, 184814777513 / 1389668723319];
+cbt = [0, 3375509829940 / 42525919076317, 272778623835 / 1039454778728, 1];
+bimbt2o = [0, 366319659506 / 1093160237145, 270096253287 / 480244073137, 104228367309 / 1017021570740];
+bexbt2o = [449556814708 / 1155810555193, 0, 210901428686 / 1400818478499, 480175564215 / 1042748212601];
+
+im_store = zeros(size(x));
+ex_store = y;
+
+exfun_denom = (dx)^2;
+imfun_denom = 2 * dx;
+for tStep= 1:Tmax / dt
+    for k=1:4 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ALL 4 RK SUBSTEPS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    dydt = -y(2:N) .* (y(3:N + 1) - y(1:N-1));
+    if (k == 1)
+        ex_store = x; 
+    else 
+        ex_store = x + (aimbt(k, k - 1) - bbt(k - 1)) * dt .* im_store + ...
+            (aexbt(k, k - 1) - bbt(k - 1)) * dt .* ex_store;
+    end
+    im_store = (eyes(4) - aimbt(k, k) * dt  .* A) \ A * ex_store;
+    ex_store = ex_store
+    end %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END OF RK LOOP %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (mod(tStep,PlotInterval)==0) NR_PlotXY(x,y,tStep*dt,0,L,-3,3); end
+end
 end % function NR_Burgers_CNRKW3_FD
