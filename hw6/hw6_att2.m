af4de3a047e4b1db8804681874a1d824f9001928
Albert Wen <albertzwen@gmail.com>
2022-12-09 08:35:16 -0800

    initial commit

---
+++ hw6/hw6_att2.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = hw6_att2(inputArg1,inputArg2)
+%HW6_ATT2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
