08242f7e192935faccebd6b09d25b226826ef3e4
Albert Wen <albertzwen@gmail.com>
2022-12-09 09:02:37 -0800

    changed main diagonal value in C, unsure of IC for v

--- hw6/hw6_att1.m
+++ hw6/hw6_att1.m
@@ -8,21 +8,24 @@ dx = L/N;
 t = 0;
 xgrid = (-N / 2:N / 2 - 1)' * dx;   % discretized points on x
 q=exp(-xgrid.^2/0.1); 
-v=0;    % v = dqdt - blanking on where to get this again
+% v=0;    % v = dqdt - blanking on where to get this again
+v = zeros(N, 1);    % is this a valid IC?
 x = [q; v]; % vector x 
 NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
-C = TTmaker(0.1, 0.1, 0.1, 2 * N);
-D = 1 / (dx)^2 * TTmaker(1.2, -2.4, 1.2, 2 * N);
-A = 1 / dt * [eye(N), zeros(N); zeros(N), C] - 0.5 * [zeros(N), eye(N); D zeros(N)];
-r = 1 / dt * [eye(N), zeros(N); zeros(N), C] + 0.5 * [zeros(N), eye(N); D zeros(N)];
+C = TTmaker(0.1, 1, 0.1, N);
+D = 1 / (dx)^2 * TTmaker(1.2, -2.4, 1.2, N);
+A = 1 / dt * [eye(N), zeros(N); zeros(N), C] ...
+    - 0.5 * [zeros(N), eye(N); D zeros(N)];
+r = 1 / dt * [eye(N), zeros(N); zeros(N), C] ...
+    + 0.5 * [zeros(N), eye(N); D zeros(N)];
 for step = 1:Tmax / dt
     x = A \ r;
     t = t + dt;
     if BCcase_a_bool
         % enforce homogenous Dirichlet BCs
-        x([1 N]) = 0;
-    else
-        % enforce periodic BCs
+        x([1 N], 1) = 0;
+%     else
+        % enforce periodic BCs- How? 
         
     end
     NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
