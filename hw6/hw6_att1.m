cbde26b61d30116a54bd5e6b3aa3c31c73230d01
Albert Wen <albertzwen@gmail.com>
2022-12-09 15:30:36 -0800

    meeting in OH resolved questions, now know where to go from here
    Wave flipping after hitting boundaries is to be expected

--- hw6/hw6_att1.m
+++ hw6/hw6_att1.m
@@ -9,7 +9,7 @@ t = 0;
 xgrid = (-N / 2:N / 2 - 1)' * dx;   % discretized points on x
 % Initial conditions
 q=exp(-xgrid.^2/0.1); 
-% v=0;    % v = dqdt - blanking on where to get this again
+% v=0;    
 v = zeros(N, 1);    % is this a valid IC?
 x = [q; v]; % vector x, q = x(1:N)
 NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
@@ -17,18 +17,17 @@ C = TTmaker(0.1, 1, 0.1, N);
 D = 1 / (dx)^2 * TTmaker(1.2, -2.4, 1.2, N);
 A = 1 / dt * [eye(N), zeros(N); zeros(N), C] ...
     - 0.5 * [zeros(N), eye(N); D, zeros(N)];
-
+rr = (1 / dt * [eye(N), zeros(N); zeros(N), C] ...
+    + 0.5 * [zeros(N), eye(N); D, zeros(N)]);
 for step = 1:Tmax / dt
     % implicit solve at each time step
-    r = (1 / dt * [eye(N), zeros(N); zeros(N), C] ...
-    + 0.5 * [zeros(N), eye(N); D, zeros(N)]) * x;
-    x = A \ r;
+    x = A \ (rr * x);   % r vector is rr * x
     t = t + dt;
     if BCcase_a_bool
         % enforce homogenous Dirichlet BCs
         x([1 N], 1) = 0;    
 %     else
-        % enforce periodic BCs- How? 
+        % enforce periodic BCs- How?  extended diagonal, circulant
         
     end
     NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
