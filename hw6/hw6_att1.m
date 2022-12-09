a74327a164516efaa114741937f9c16c4188659a
Albert Wen <albertzwen@gmail.com>
2022-12-09 08:15:59 -0800

    added Dirichlet BCs

--- hw6/hw6_att1.m
+++ hw6/hw6_att1.m
@@ -1,18 +1,26 @@
-function Wave1D_CN_Pade(L, Tmax, c, N, dt)
+function hw6_att1(L, Tmax, N, dt)
+% Solves the 1D wave equation using CN in time, compact Pade in space
 % L = dimension width
 % Tmax = total simulation time
 % N = number of gridpoint discretizations
 % dt = time step size
 dx = L/N; 
-IterSteps = 2;
 t = 0;
-x = (-N/2:N/2-1)'*dx; 
-q=exp(-x.^2/0.1); v=0;
-NR_PlotXY(x,q,t,-L/2,L/2,-0.2,1.2); 
-vs=v; 
-qs=v; 
-a=0.6*dt*c^2/dx^2; 
-b=-1.2*dt*c^2/dx^2;
-for 
+xgrid = (-N / 2:N / 2 - 1)' * dx;   % discretized points on x
+q=exp(-xgrid.^2/0.1); 
+v=0;    % v = dqdt - blanking on where to get this again
+x = [q; v]; % vector x 
+NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
+C = TTmaker(0.1, 0.1, 0.1, 2 * N);
+D = 1 / (dx)^2 * TTmaker(1.2, -2.4, 1.2, 2 * N);
+A = 1 / dt * [eye(N), zeros(N); zeros(N), C] - 0.5 * [zeros(N), eye(N); D zeros(N)];
+r = 1 / dt * [eye(N), zeros(N); zeros(N), C] + 0.5 * [zeros(N), eye(N); D zeros(N)];
+for step = 1:Tmax / dt
+    x = A \ r;
+    t = t + dt;
+    % enforce homogenous BCs
+    x([1 N]) = 0;
+    NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
+end
 end
 
