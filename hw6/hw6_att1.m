4aca5e08d9c62c0e7ab10c8a45f99fccc3e64de8
Albert Wen <albertzwen@gmail.com>
2022-12-09 10:33:00 -0800

    realized r was not being calculated correctly, and a few commas

--- hw6/hw6_att1.m
+++ hw6/hw6_att1.m
@@ -7,23 +7,26 @@ function hw6_att1(L, Tmax, N, dt, BCcase_a_bool)
 dx = L/N; 
 t = 0;
 xgrid = (-N / 2:N / 2 - 1)' * dx;   % discretized points on x
+% Initial conditions
 q=exp(-xgrid.^2/0.1); 
 % v=0;    % v = dqdt - blanking on where to get this again
 v = zeros(N, 1);    % is this a valid IC?
-x = [q; v]; % vector x 
+x = [q; v]; % vector x, q = x(1:N)
 NR_PlotXY(xgrid,x(1:N),t,-L/2,L/2,-0.2,1.2); 
 C = TTmaker(0.1, 1, 0.1, N);
 D = 1 / (dx)^2 * TTmaker(1.2, -2.4, 1.2, N);
 A = 1 / dt * [eye(N), zeros(N); zeros(N), C] ...
-    - 0.5 * [zeros(N), eye(N); D zeros(N)];
-r = 1 / dt * [eye(N), zeros(N); zeros(N), C] ...
-    + 0.5 * [zeros(N), eye(N); D zeros(N)];
+    - 0.5 * [zeros(N), eye(N); D, zeros(N)];
+
 for step = 1:Tmax / dt
+    % implicit solve at each time step
+    r = (1 / dt * [eye(N), zeros(N); zeros(N), C] ...
+    + 0.5 * [zeros(N), eye(N); D, zeros(N)]) * x;
     x = A \ r;
     t = t + dt;
     if BCcase_a_bool
         % enforce homogenous Dirichlet BCs
-        x([1 N], 1) = 0;
+        x([1 N], 1) = 0;    
 %     else
         % enforce periodic BCs- How? 
         
