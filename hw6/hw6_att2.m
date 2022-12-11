7e6621124c0f50ae5290a04ce0a7d7131e89e34a
Albert Wen <albertzwen@gmail.com>
2022-12-10 16:54:05 -0800

    determined that there was a mistake in assignment notes - third subdiagonal alternates with 0

--- hw6/hw6_att2.m
+++ hw6/hw6_att2.m
@@ -15,14 +15,14 @@ x = zeros(2 * N, 1);
 x(1:2:end) = q;
 NR_PlotXY(xgrid,x(1:2:end),t,-L/2,L/2,-0.2,1.2); 
 %% Computing E and F
-F = 1.2 / dx^2 * diag(ones(2 * N - 3, 1), -3) ...
+F = 1.2 / dx^2 * diag(mod(1:2 * N - 3, 2), -3) ...
     - 2.4 / dx^2 * diag(mod(1:2 * N - 1, 2), -1) ...
-    + diag(mod(1:2 * N - 1, 2), 1) + 1.2 /dx^2 * diag(mod(0:2 * N - 1 - 1, 2), 1);
+    + (diag(mod(1:2 * N - 1, 2), 1) + 1.2 /dx^2 * diag(mod(0:2 * N - 1 - 1, 2), 1));
 E = 0.1 * diag(mod(0:2 * N - 2 - 1, 2), -2) ...
     + diag(ones(2 * N, 1), 0) ...
     + 0.1 * diag(mod(0:2 * N - 2 - 1, 2), 2);
-A = (1 / dt * E) - (0.5 * F);
-rr = (1 / dt * E) + (0.5 * F);
+A = (E / dt) - (F / 2);
+rr = (E / dt) + (F / 2);
 for step = 1:Tmax / dt
     x = A \ (rr * x);
     t = t + dt;
@@ -32,4 +32,3 @@ for step = 1:Tmax / dt
     NR_PlotXY(xgrid,x(1:2:end),t,-L/2,L/2,-0.2,1.2); 
 end
 end
-
